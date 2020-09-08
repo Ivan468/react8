3368dde73dd875dd6941bcbd33d5f216c31019ea
Ivan <pig1995z@gmailcom>
2020-09-08 20:59:59 +0300

    del comments

--- src/App.js
+++ src/App.js
@@ -7,41 +7,9 @@ class App extends React.Component {
   render(){
     return (
       <div>
-       
           <Project/>
       </div>
     )
   }
 }
-
-// <<<<<<< HEAD
-
-
-
-// =======
-// export default App;
-
-
-// >>>>>>> ebf2ce646c4c3c772d58d7b2660297179af3efe2
-// function AppFunc() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
 export default App;
