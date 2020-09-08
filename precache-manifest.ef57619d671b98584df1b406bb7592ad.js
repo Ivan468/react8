f012565814f08e6d18e55452b14db8bcb7394023
Ivan <pig1995z@gmailcom>
2020-09-08 19:13:45 +0300

    Updates

---
+++ precache-manifest.ef57619d671b98584df1b406bb7592ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3a33dcbbce05a9ccb50d52c1ac3647c",
+    "url": "/react8/index.html"
+  },
+  {
+    "revision": "de930ae7bd8d563c2fd9",
+    "url": "/react8/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7f0a393bfd7b4931ebb1",
+    "url": "/react8/static/js/2.73778eca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react8/static/js/2.73778eca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de930ae7bd8d563c2fd9",
+    "url": "/react8/static/js/main.9c755b84.chunk.js"
+  },
+  {
+    "revision": "cfd277f4c1c45649c25e",
+    "url": "/react8/static/js/runtime-main.30514cf2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react8/static/media/logo.5d5d9eef.svg"
+  }
+]);
