0ad6eb139841009cd4c24032de6072944e0cf0db
antMaks <maksimantonuk74@gmail.com>
2020-09-09 09:09:12 +0300

    Update TestRename.js

--- src/TestRename/TestRename.js
+++ src/TestRename/TestRename.js
@@ -4,7 +4,7 @@ export default class Project extends Component {
     render() {
         return(
             <div>
-                <h1> Привет ребят! <br/> Мы сделаем это! </h1>
+                <h1 style={{color:'red'}} > Привет ребят! <br/> Мы сделаем это! </h1>
                 <h3>Test push</h3>
             </div>
         )
