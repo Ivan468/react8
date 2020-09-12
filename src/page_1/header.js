4014c9de2f6987e64157e330b67db8798f6f8e84
Ivan <pig1995z@gmailcom>
2020-09-12 14:06:43 +0300

    routing add

--- src/page_1/header.js
+++ src/page_1/header.js
@@ -1,31 +1,23 @@
 import React, { Component } from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Table from "./table";
 
 class Header extends Component {
   render() {
     return (
       <Router>
-      <div className="header">
-        <div className="logo">
+        <div className="header">
+          <div className="logo">
             <h3>MoneyTracker</h3>
+          </div>
+
+          <div className="menu">
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/dashboard">Categories</Link>
+          </div>
         </div>
 
-        <div className="menu">
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/dashboard">Dashboard</Link>
-        </div>
-      </div>
-
-
-       
-      
         <Switch>
           <Route exact path="/">
             <Home />
@@ -34,11 +26,9 @@ class Header extends Component {
             <About />
           </Route>
           <Route path="/dashboard">
-            <Dashboard />
+            <Incomes />
           </Route>
         </Switch>
-       
-
       </Router>
     );
   }
@@ -46,50 +36,60 @@ class Header extends Component {
 
 function Home() {
   return (
-    <div className="right-block">
-     <div >
-            <div className="balanse">
-              <h4>balanse</h4>
-              <h5>$2,651.07</h5>
-            </div>
-            <div>
-              <div className="href-right-block">
-                <div className="Charges">
-                  <Link to="/header"> Charges</Link>
-                </div>
-                <div className="Incomes">
-                  <Link to="/"> Incomes</Link>
-                </div>
-              </div>
-              <div className="charges">
-                <div>
-                  <span className="my-charges">My Charges</span>
-                  <select className="select-p-1">
-                    <option>this week</option>
-                    <option>this month</option>
-                  </select>
-                </div>
-                <button>Add more</button>
-              </div>
-              <Table />
-            </div>
+    <Router>
+      <div className="right-block">
+        <div>
+          <div className="balanse">
+            <h4>balanse</h4>
+            <h5>$2,651.07</h5>
           </div>
-    </div>
+          <div>
+            <div className="href-right-block">
+              <div className="Charges">
+                <Link to="/charges"> Charges</Link>
+              </div>
+              <div className="Incomes">
+                <Link to="/incomes"> Incomes</Link>
+              </div>
+            </div>
+            <div className="charges">
+              <div>
+                <span className="my-charges">My Charges</span>
+                <select className="select-p-1">
+                  <option>this week</option>
+                  <option>this month</option>
+                </select>
+              </div>
+              <button>Add more</button>
+            </div>
+            <Switch>
+              <Route path="/charges">
+                 <Table />
+              </Route>
+              <Route path="/incomes">
+              <Incomes />
+
+              </Route>
+            </Switch>
+          </div>
+        </div>
+      </div>
+    </Router>
   );
 }
 
 function About() {
   return (
     <div>
-      <h2>About</h2>
+          <h1> about</h1>
     </div>
   );
 }
 
-function Dashboard() {
+function Incomes() {
   return (
     <div>
-      <h2>Dashboard</h2>
+      <h2>incomes</h2>
     </div>
   );
 }
