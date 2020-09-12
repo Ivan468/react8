5399cd6793cf8a8f7ad101d2244265cea4b43948
antMaks <maksimantonuk74@gmail.com>
2020-09-12 13:05:36 +0300

    add header

--- src/App.js
+++ src/App.js
@@ -1,47 +1,13 @@
 import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-import Project from './TestRename/TestRename'
+import Header from '../src/Header/Header'
 
-class App extends React.Component {
-  render(){
-    return (
-      <div>
-      <h1>hello world!!!!</h1>
-          <Project/>
-      </div>
-    )
-  }
+
+const App = () => {
+  return (
+    <div>
+    <Header/>
+    </div>
+  )
 }
-
-// <<<<<<< HEAD
-
-
-
-// =======
-// export default App;
-
-
-// >>>>>>> ebf2ce646c4c3c772d58d7b2660297179af3efe2
-// function AppFunc() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
