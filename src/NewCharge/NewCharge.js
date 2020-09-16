7b80f4a04c2d8aeb69e5b69b64f1b09e8bfef32b
antMaks <maksimantonuk74@gmail.com>
2020-09-16 19:10:46 +0300

    code

--- src/NewCharge/NewCharge.js
+++ src/NewCharge/NewCharge.js
@@ -1,5 +1,54 @@
-import React, { Component } from 'react'
+import React from 'react';
+import './NewCategory.css';
+import './NewCategory.scss';
+import Selectapp from  './selectApp'
 
-function NewCharge() {
-    
+
+
+
+function Charts() {
+  return (
+    <div className="charts">
+     <h3>Надпис</h3>
+    <input/>
+    <h3>Надпис</h3>
+    <input/>
+    <h3>Надпис</h3>
+       <Selectapp/>
+       <button>Knopka</button>
+    </div>
+  );
 }
+
+const NewCharge = (props) => {
+  return (
+    <div className="new-category__block">
+    <h3 className="category-header__name">New category</h3>
+    <div className="category-main__block">
+    <span className="category-main__name">Name</span>
+    <div className="category-main__input">
+
+    </div>
+
+    <span className="category-main__name">Descripson</span>
+    <div className="category-main__input">
+
+
+    </div>
+    <div className="category-main__icon"></div>
+    <span className="category-main__name">Select icon</span>
+    <div className="category-main__input">
+
+    </div>
+
+    <button className="category-main__addButton">Add new category</button>
+    </div>
+    </div>
+
+    
+
+  
+  )
+}
+
+export default NewCategory;
