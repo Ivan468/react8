047ec2d9bc26cba2dff08ee2ecff38459e2c604a
antMaks <maksimantonuk74@gmail.com>
2020-09-16 16:13:51 +0300

    Create header.js

---
+++ src/page_1/header.js
@@ -0,0 +1,96 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Table from "./table";
+import Selectapp from "./select";
+ 
+
+class Header extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="header">
+          <div className="logo">
+            <h3>MoneyTracker</h3>
+          </div>
+          <div className="menu">
+            <Link to="/">Home</Link>
+            <Link to="/charts">Charts</Link>
+            <Link to="/dashboard">Categories</Link>
+          </div>
+        </div>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/charts">
+            <Charts />
+          </Route>
+          <Route path="/dashboard">
+            <Incomes />
+          </Route>
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+function Home() {
+  return (
+    <Router>
+      <div className="right-block">
+        <div>
+          <div className="balanse">
+            <h4>balanse</h4>
+            <h5>$2,651.07</h5>
+          </div>
+          <div>
+            <div className="href-right-block">
+              <div className="Charges">
+                <Link to="/charges"> Charges</Link>
+              </div>
+              <div className="Incomes">
+                <Link to="/incomes"> Incomes</Link>
+              </div>
+            </div>
+            <div className="charges">
+              <div>
+                <span className="my-charges">My Charges</span>
+                <select className="select-p-1">
+                  <option>this week</option>
+                  <option>this month</option>
+                </select>
+              </div>
+              <button>Add more</button>
+            </div>
+            <Switch>
+              <Route path="/charges">
+                <Table />
+              </Route>
+              <Route path="/incomes">
+                <Incomes />
+              </Route>
+            </Switch>
+          </div>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+function Charts() {
+  return (
+    <div className="charts">
+       <Selectapp/>
+    </div>
+  );
+}
+
+function Incomes() {
+  return (
+    <div>
+      <h2>incomes</h2>
+    </div>
+  );
+}
+
+export default Header;
